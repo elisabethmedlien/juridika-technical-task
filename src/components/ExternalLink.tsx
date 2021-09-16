b935b7278295790439b1ede6ef3f5fb1f36dcc9e
Elisabeth Medlien <elisabethmedlien@gmail.com>
2021-09-16 11:49:57 +0200

    removing warnings

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -1,6 +1,6 @@
 const ExternalLink = (props:any) => {
   return(
-    <a href={props.path} rel="nofollow" target="_blank">
+    <a href={props.path} rel="noreferrer" target="_blank">
       {props.children}
     </a>
   )
--- src/functions/common.tsx
+++ src/functions/common.tsx
@@ -1,7 +1,6 @@
 const url:string = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=100';
 
-export async function getApi <Response> (
-): Promise <any> {
+export async function getApi (): Promise <any> {
   try {
     const response = await fetch(url);
     return await response.json();
